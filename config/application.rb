bb8746d27ebe342684ba2b6d725ffaae066f670c
Mika <mika@matsunagamikanoMacBook-Pro.local>
2020-10-22 20:24:22 +0900

    [add]active_adminとuserログイン新規登録

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module EGC
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.assets.paths << config.root.join("vendor/assets/javascripts")
+    config.assets.paths << config.root.join("vendor/assets/stylesheets")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
