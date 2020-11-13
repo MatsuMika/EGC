7b497ee704eee202f78c778a0f5e82b55488e73e
vagrant <vagrant@localhost.localdomain>
2020-11-13 12:20:29 +0000

    [update]layout_Indent

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,12 @@
 class SearchController < ApplicationController
 	def search
-		@keyword = params[:title]
-		@posts = Post.where('title like ?',"%#{@keyword}%")
+		@type = params[:type]
+		@keyword = params[:keyword]
+		@posts = if @type == 'user'
+					Post.joins(:user).where('users.nick_name like ?', "%#{@keyword}%")
+				 else
+				 	Post.joins(:user).where('title like ? or subtitle like ? or development_environment_text like ?', "%#{@keyword}%", "%#{@keyword}%", "%#{@keyword}%")
+				 end
 		render "posts/index"
 	end
 end
