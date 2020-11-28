b8fd5415f7b0a82393f1709e7e45b32c8ab2577c
vagrant <vagrant@localhost.localdomain>
2020-11-28 04:16:19 +0000

    [add]carrierwave_S3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,24 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+# 画像名/日本語可
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'egc'
+    config.fog_public     = false
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1',
+      path_style: true
+    }
+  else
+    config.storage :file
+  end
+end
