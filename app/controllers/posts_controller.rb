8cf68b1faa687bb7b7589c519b1b527416ff3acc
vagrant <vagrant@localhost.localdomain>
2020-12-03 13:16:35 +0000

    [Improve]posts_controller.rb.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
 	end
 
   def index
-  	@posts = Post.page(params[:page]).per(6)
+  	@posts = Post.order(created_at: :desc).page(params[:page]).per(6)
   end
 
   def show
