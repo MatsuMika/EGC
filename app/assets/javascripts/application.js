f69c975502213d0633f33ddfe6cf2f58c2854ad4
vagrant <risa.mkwork314@gmail.com>
2021-01-18 19:22:56 +0000

    [Update]view/top/animation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,3 +26,13 @@ $(document).on('turbolinks:load',function() {
     event.preventDefault();
   });
 });
+
+window.onload = function() {
+  Particles.init({
+    selector: '.background',
+    sizeVariations: 30,
+    color: [
+      '#0bd', 'rgba(0,187,221,.5)', 'rgba(0,187,221,.2)'
+    ]
+  });
+};
